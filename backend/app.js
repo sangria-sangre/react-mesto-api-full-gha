c8d78017abf6e70e5d42958c6a28ec866778327d
sangria-sangre <114749225+sangria-sangre@users.noreply.github.com>
2023-06-29 01:30:15 +0300

    Исправление ошибки pm2

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const helmet = require('helmet');
 const rateLimit = require('express-rate-limit')
 const { errors } = require('celebrate');
 const errorHandler = require('./middlewares/error-handler');
-const cors = require('cors');
+//const cors = require('cors');
 
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
@@ -21,7 +21,7 @@ const {
 } = process.env;
 
 const app = express();
-app.use(cors());
+//app.use(cors());
 
 app.use(helmet());
 app.use(limiter);
